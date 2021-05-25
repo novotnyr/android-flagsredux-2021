1f24074d725bb8229370e6a9a285e7a0f316fd85
Robert Novotny <novotnr0@gmail.com>
2021-05-26 00:21:26 +0200

    Prepojenie adaptera a recyclerview
    Naplnenie adaptera vlajkami

--- app/src/main/java/com/github/novotnyr/android/flagsredux/FlagListFragment.kt
+++ app/src/main/java/com/github/novotnyr/android/flagsredux/FlagListFragment.kt
@@ -10,5 +10,22 @@ class FlagListFragment : Fragment(R.layout.fragment_flag_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         flagRecyclerView = view.findViewById(R.id.flagRecyclerView)
+        flagRecyclerView.adapter = FlagListAdapter {
+            TODO("Dopracovať klik na vlajku")
+        }.apply {
+            submitList(listOf(
+                Flag("Austria", R.drawable.austria),
+                Flag("Croatia", R.drawable.croatia),
+                Flag("Czech Republic", R.drawable.czech),
+                Flag("Germany", R.drawable.germany),
+                Flag("Hungary", R.drawable.hungary),
+                Flag("Poland", R.drawable.poland),
+                Flag("Romania", R.drawable.romania),
+                Flag("Serbia", R.drawable.serbia),
+                Flag("Slovakia", R.drawable.slovakia),
+                Flag("Slovenia", R.drawable.slovenia),
+                Flag("Ukraine", R.drawable.ukraine),
+            ))
+        }
     }
 }
