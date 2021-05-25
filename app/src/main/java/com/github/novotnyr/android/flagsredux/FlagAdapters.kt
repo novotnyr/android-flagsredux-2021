74862a0faac2128ce833579d580e6d75869fe99b
Robert Novotny <novotnr0@gmail.com>
2021-05-25 23:59:24 +0200

    Pridany ViewHolder

--- app/src/main/java/com/github/novotnyr/android/flagsredux/FlagAdapters.kt
+++ app/src/main/java/com/github/novotnyr/android/flagsredux/FlagAdapters.kt
@@ -1,6 +1,10 @@
 package com.github.novotnyr.android.flagsredux
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
 
 object FlagDiff : DiffUtil.ItemCallback<Flag>() {
     override fun areItemsTheSame(old: Flag, new: Flag): Boolean {
@@ -12,3 +16,12 @@ object FlagDiff : DiffUtil.ItemCallback<Flag>() {
     }
 }
 
+class FlagViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val flagImageView: ImageView = itemView.findViewById(R.id.flagImageView)
+    val countryTextView: TextView = itemView.findViewById(R.id.countryTextView)
+
+    fun bind(flag: Flag) {
+        flagImageView.setImageResource(flag.flagResource)
+        countryTextView.text = flag.country
+    }
+}
