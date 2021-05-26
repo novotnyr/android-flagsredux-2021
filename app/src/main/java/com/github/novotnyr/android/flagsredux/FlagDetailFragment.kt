7efaee3b0e86b3063bf7cc36328bd39ae2554cb4
Robert Novotny <novotnr0@gmail.com>
2021-05-26 10:21:47 +0200

    Vlajka sa zobrazuje na zaklade argumentov

--- app/src/main/java/com/github/novotnyr/android/flagsredux/FlagDetailFragment.kt
+++ app/src/main/java/com/github/novotnyr/android/flagsredux/FlagDetailFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 class FlagDetailFragment : Fragment(R.layout.fragment_flag_detail) {
     private lateinit var flagImageView: ImageView
 
+    private val args: FlagDetailFragmentArgs by navArgs()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         flagImageView = view.findViewById(R.id.flagImageView)
+        flagImageView.setImageResource(args.flag.flagResource)
     }
 }
