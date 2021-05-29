e0f289040350858ec03f4143389920bdfd32814c
Robert Novotny <novotnr0@gmail.com>
2021-05-29 15:04:10 +0200

    Rozhybanie prichadzajuceho fragmentu

--- app/src/main/java/com/github/novotnyr/android/flagsredux/FlagDetailFragment.kt
+++ app/src/main/java/com/github/novotnyr/android/flagsredux/FlagDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.transition.MaterialContainerTransform
 
 class FlagDetailFragment : Fragment(R.layout.fragment_flag_detail) {
     private lateinit var flagImageView: ImageView
@@ -15,5 +16,7 @@ class FlagDetailFragment : Fragment(R.layout.fragment_flag_detail) {
         flagImageView = view.findViewById(R.id.flagImageView)
         flagImageView.setImageResource(args.flag.flagResource)
         flagImageView.transitionName = args.flag.country
+
+        sharedElementEnterTransition = MaterialContainerTransform(requireContext(), true)
     }
 }
