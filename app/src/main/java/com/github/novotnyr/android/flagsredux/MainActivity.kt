ddc8eb594703e9e04d8d70cc9e12dcaa14e7889a
Robert Novotny <novotnr0@gmail.com>
2021-05-29 16:38:47 +0200

    Workaround chyby v kniznici Android Material, ktora brani korektnej spatnej animacii

--- app/src/main/java/com/github/novotnyr/android/flagsredux/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/android/flagsredux/MainActivity.kt
@@ -2,9 +2,13 @@ package com.github.novotnyr.android.flagsredux
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // https://github.com/material-components/material-components-android/issues/2215#issuecomment-847033187
+        FragmentManager.enableNewStateManager(false)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
